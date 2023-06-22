eb9eaf0539add10ed21cb9305e45694e923e6d35
Jonathan Christofoleti <jonafoleti@gmail.com>
2023-06-22 14:36:20 -0300

    botao nao funciona

--- frontend/src/telas/Init.tsx
+++ frontend/src/telas/Init.tsx
@@ -37,13 +37,16 @@ export default function Init() {
       <div className="info">
         <span className="spanInfo">
           <button className="glow-on-hover" onClick={toggleWiki}>Wiki</button>
-          <span id="wiki" className="glow-on-hover">
-            As expressões faciais são formas de comunicação humana que transmitem diversas emoções, como alegria, tristeza, raiva, medo e surpresa. Elas são formadas pela contração dos músculos do rosto e podem ser involuntárias ou voluntárias, dependendo do contexto.
-            Existem seis emoções básicas universalmente reconhecidas, mas a expressão de neutralidade também pode ser considerada uma emoção básica.
-            Nos últimos anos, houve avanços na detecção de expressões faciais por meio de algoritmos e técnicas de aprendizado de máquina, como redes neurais.
-            Essa tecnologia tem diversas aplicações, desde análise de bem-estar social até criação de interfaces para interação humano-computador, colaboração para diagnósticos médicos e coleta de feebacks de determinados produtos e serviços oferecidos a clientes e usuários.
-            Este trabalho propõe o desenvolvimento de um algorítmo capaz de detectar, uma foto, um rosto humano e, a partir deste rosto, analisar e descobrir qual expressão facial ele está emitindo naquele momento para, assim, detectar seu respectivo sentimento.
-          </span>
+          {wikiVisible ? 
+            <span id="wiki" className="glow-on-hover">
+              As expressões faciais são formas de comunicação humana que transmitem diversas emoções, como alegria, tristeza, raiva, medo e surpresa. Elas são formadas pela contração dos músculos do rosto e podem ser involuntárias ou voluntárias, dependendo do contexto.
+              Existem seis emoções básicas universalmente reconhecidas, mas a expressão de neutralidade também pode ser considerada uma emoção básica.
+              Nos últimos anos, houve avanços na detecção de expressões faciais por meio de algoritmos e técnicas de aprendizado de máquina, como redes neurais.
+              Essa tecnologia tem diversas aplicações, desde análise de bem-estar social até criação de interfaces para interação humano-computador, colaboração para diagnósticos médicos e coleta de feebacks de determinados produtos e serviços oferecidos a clientes e usuários.
+              Este trabalho propõe o desenvolvimento de um algorítmo capaz de detectar, uma foto, um rosto humano e, a partir deste rosto, analisar e descobrir qual expressão facial ele está emitindo naquele momento para, assim, detectar seu respectivo sentimento.
+            </span>
+          : null
+          }
         </span>
         <span className="spanInfo">
           <button className="glow-on-hover" onClick={toggleDevs}>Devs</button>
