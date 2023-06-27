283417c6c8caf883479b405529bc2b42423ce5f9
Felipe Bueno de Souza <felipebuenosouza@uol.com.br>
2023-06-27 13:08:50 -0300

    Update Classifier.tsx

--- frontend/src/telas/Classifier.tsx
+++ frontend/src/telas/Classifier.tsx
@@ -1,21 +1,23 @@
 import React, { useState, ChangeEvent } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
 
 export default function Algoritmo() {
-    const [pictureImage, setPictureImage] = useState('Escolha uma imagem');
+
+    const [emotion, setEmotion] = useState<string>("");
+    const [pictureImage, setPictureImage] = useState<string>("");
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
-
         if (file) {
             const reader = new FileReader();
-
+            getEmotion(file)
             reader.addEventListener('load', (e) => {
                 const readerTarget = e.target as FileReader;
-
                 if (readerTarget) {
                     const imgDataUrl = readerTarget.result as string;
                     setPictureImage(imgDataUrl);
-                    //downloadImage(imgDataUrl);
+                    // downloadImage(imgDataUrl);
                 }
             });
 
@@ -25,6 +27,7 @@ export default function Algoritmo() {
         }
     };
 
+    // eslint-disable-next-line
     const downloadImage = (imgDataUrl: string) => {
         const downloadLink = document.createElement('a');
         downloadLink.href = imgDataUrl;
@@ -35,14 +38,41 @@ export default function Algoritmo() {
         downloadLink.remove();
     };
 
-    return (
-        <>
-            <label className="picture" htmlFor="picture__input">
-                <img src={pictureImage} alt="Selecione uma Imagem" style={{width:'100%', height:'100%'}}/>
-            </label>
 
-            <input type="file" name="picture__input" id="picture__input" onChange={handleFileChange}></input>
-        </>
+
+    function getEmotion(image: any) {
+        const formData = new FormData();
+        formData.append('imagefile', image);
+        const headers: HeadersInit = {
+            'Access-Control-Allow-Origin': '*'
+        }
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            headers,
+            body: formData
+        };
+        fetch("http://localhost:5000/get-image", requestOptions)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                setEmotion(res.message)
+            })
+            .catch(e => console.error(e))
+    }
+
+    return (
+        <Row className="justify-content-center">
+            <Col md={6}>
+                <label className="picture" htmlFor="picture__input">
+                    <img alt="teste" width="100%" height="100%" src={pictureImage} />
+                </label>
+
+                <input type="file" name="picture__input" id="picture__input"
+                    onChange={handleFileChange} />
+                <h1>{emotion}</h1>
+            </Col>
+
+        </Row>
 
 
     );
